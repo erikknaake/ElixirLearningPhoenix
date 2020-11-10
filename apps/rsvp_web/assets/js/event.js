46e5598e50798b62818d708e86ac1b9cace1466d
Erik Knaake <e.knaake@student.han.nl>
2020-11-10 19:31:29 +0100

    join channel

---
+++ apps/rsvp_web/assets/js/event.js
@@ -0,0 +1,18 @@
+import socket from './socket';
+
+(function() {
+    let id = $('#id').data('id');
+    if(!id) {
+        return;
+    }
+
+    let channel = socket.channel("event:" + id, {});
+
+    channel.join()
+        .receive("ok", resp => {
+            console.log("Joined successfully, event: " + id, resp);
+        })
+        .receive("error", resp => {
+            console.log('Unable to join, response: ', resp);
+        });
+})();
