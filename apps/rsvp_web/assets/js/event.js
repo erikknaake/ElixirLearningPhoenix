2d28d760dc27367f611805bdb5125fcec7b06f86
Erik Knaake <e.knaake@student.han.nl>
2020-11-10 20:16:28 +0100

    channels

--- apps/rsvp_web/assets/js/event.js
+++ apps/rsvp_web/assets/js/event.js
@@ -8,6 +8,11 @@ import socket from './socket';
 
     let channel = socket.channel("event:" + id, {});
 
+    channel.on("update_quantity", event => {
+        console.log("update", event);
+        $('h4 span').text(event.quantity);
+    });
+
     channel.join()
         .receive("ok", resp => {
             console.log("Joined successfully, event: " + id, resp);
